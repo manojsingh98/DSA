f7f6df416166ff8f2f6e0b7175a4553493f98c3d
manoj singh <manoj.singh@techchefz.com>
2024-01-22 20:28:45 +0530

    get odda and even values

---
+++ find odd and even values/index.js
@@ -0,0 +1,11 @@
+let even = []
+let odd = []
+
+for (let i = 1; i <= 100; i++) {
+if(i % 2 == 0){
+    even.push(i)
+} else (odd.push(i))
+
+}
+console.info("even values :" ,even);
+console.warn("odd values :",odd);
